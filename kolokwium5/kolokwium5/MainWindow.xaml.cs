83dc19b0131ad2bea9b61f005dcc73d3e2f56ee4
Jakub Mida <147977475+JakubMida@users.noreply.github.com>
2025-04-24 08:25:39 +0200

    Kolokwium

---
+++ kolokwium5/kolokwium5/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kolokwium5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MojStos<string> stos = new MojStos<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, RoutedEventArgs e)
+        {
+            stos.DodajNaGore("Jeden");
+            stos.DodajNaGore("Dwa");
+            stos.DodajNaGore("Trzy");
+            stos.DodajNaGore("Cztery");
+            stos.DodajNaGore("Pięć");
+        }
+
+        private void btnZdejmij_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string element = stos.ZdejmijZGory();
+                lista.Items.Add(element);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ kolokwium5/kolokwium5/MojStos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kolokwium5
+{
+    public class MojStos<T>
+    {
+        List<T> dane = new List<T>();
+
+        public void DodajNaGore(T element)
+        {
+            dane.Add(element);
+        }
+
+        public T ZdejmijZGory()
+        {
+            if (dane.Count > 0)
+            {
+                int index = dane.Count - 1;
+                var element = dane[index];
+                dane.RemoveAt(index);
+                return element;
+            }
+            else
+            {
+                throw new ArgumentException("Stos jest pusty");
+            }
+
+        }
+    }
+}
